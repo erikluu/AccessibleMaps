19c701400760def90953e216889409150bcb4ab6
Erik Luu <eeluu@calpoly.edu>
2023-03-01 14:30:09 -0800

    refactoring

--- src/App.js
+++ src/App.js
@@ -11,27 +11,6 @@ import MapView from "./components/MapView";
 
 function App() {
 
-  const [waypoints, setWaypoints] = useState([]);
-  const [routePath, setRoutePath] = useState([]);
-  const [routeDistance, setRouteDistance] = useState(0);
-  const [routeDistanceUnit, setRouteDistanceUnit] = useState('mi'); // 'km' or 'mi'
-
-  const waypoint0 = 'Cal Poly, CA';
-  const waypoint1 = '776 Chorro St, San Luis Obispo, CA';
-
-  useEffect(() => {
-    axios.get(`http://localhost:4000/api/route?wp0=${waypoint0}&wp1=${waypoint1}&unit=${routeDistanceUnit}`)
-      .then(response => {
-        setRoutePath(response.data.resourceSets[0].resources[0].routePath.line.coordinates);
-        setRouteDistance(response.data.resourceSets[0].resources[0].travelDistance);
-        setRouteDistanceUnit(response.data.resourceSets[0].resources[0].travelDistanceUnit);
-        
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <div>
       <MapView />
