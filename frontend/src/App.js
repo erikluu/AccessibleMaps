7979e8dac84b3b18c3fd0d5c3ea703c306919614
crugggiero <ruggicar555@gmail.com>
2023-04-12 13:53:15 -0700

    add sidebar back in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import axios from 'axios';
+import SideBar from "./components/SideBar";
 import MapView from "./components/MapView";
 
 <link
@@ -8,8 +12,14 @@ import MapView from "./components/MapView";
 
 function App() {
 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const handleViewSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div>
+      <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
       <MapView />
     </div>
   );
--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Path from "./Path";
 
 const SideBar = props => {
